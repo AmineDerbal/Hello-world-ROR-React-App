90b394015e3cd4babc1f67bb94920efceeb917bc
Amine derbal <aminedoran31@gmail.com>
2023-07-10 15:22:20 +0100

    Update files to fix linters

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,3 @@
 class Message < ApplicationRecord
-validates :message, presence: true, length: {minimum: 5, maximum: 100}
-
+  validates :message, presence: true, length: { minimum: 5, maximum: 100 }
 end
